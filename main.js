2e69037270318c55cce6628f869c8466e8cafe64
andertakecare <20363451+andertakecare@users.noreply.github.com>
2023-10-21 20:10:42 +0900

    implement Field class initial constructor and print method

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+
+class Field {
+    constructor(field) {
+        this.field = field;
+    }
+
+    print() {
+        for (let i = 0; i < this.field.length; i++) {
+            for (let j = 0; j < this.field[i].length; j++){
+                console.log(this.field[i][j].join(''));
+            }
+            console.log('\n');
+        }
+    }
+}
